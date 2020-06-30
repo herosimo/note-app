c53ca30ca2bc7322665c33f38f0bbea160ecfb07
Herosimo Sribiko <herosimo06@gmail.com>
2020-06-30 10:55:16 +0700

    Updates

---
+++ precache-manifest.867f0eb4e3a5e5c5b8b6b9c0e1d871a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d0b60f330895b81b6378ae3856cd83e",
+    "url": "/note-app/index.html"
+  },
+  {
+    "revision": "5a51dba4072164eb7173",
+    "url": "/note-app/static/css/main.b74c17dd.chunk.css"
+  },
+  {
+    "revision": "6967c760ff93496f5303",
+    "url": "/note-app/static/js/2.5623a3fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/note-app/static/js/2.5623a3fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a51dba4072164eb7173",
+    "url": "/note-app/static/js/main.dbfe33f9.chunk.js"
+  },
+  {
+    "revision": "8a1a8e1c8cd7f2be9b78",
+    "url": "/note-app/static/js/runtime-main.7f39f972.js"
+  }
+]);
